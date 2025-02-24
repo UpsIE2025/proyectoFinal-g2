f0419116609c91f92a687124309d6dfefe477df0
jonathan <jonathans2200@hotmail.com>
2025-02-24 15:57:03 -0500

    pruebas de replicacion final

--- services/micro-cdc/script/init.sql
+++ services/micro-cdc/script/init.sql
@@ -16,3 +16,7 @@ curl -H "Accept:application/json" localhost:8083/connectors/
 
  curl -s http://localhost:8083/connectors/postgres-connector/status
 INSERT INTO cuentas (id, estado, fecha_creacion, saldo, usuario_id) VALUES (1, 'activo', '2025-02-23', 1000.50, 123);
+
+
+
+INSERT INTO consumos (id, cuenta_id, descripcion, estado, fecha_consumo, monto) VALUES (1, 13, 'Compra en tienda', 'activo', '2023-10-25 14:30:00', 100.50);
