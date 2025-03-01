41774c3db883b145eab97d94a67139337379b8e2
jonathan <jonathans2200@hotmail.com>
2025-03-01 09:05:01 -0500

    correccion final del docker compose

--- services/micro-cdc/script/init.sql
+++ services/micro-cdc/script/init.sql
@@ -12,11 +12,11 @@ CREATE PUBLICATION dbz_publication FOR ALL TABLES;
 -- curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://localhost:8083/connectors/ -d @services/micro-cdc/debezium-connector/register-connector.json
 
 
-curl -H "Accept:application/json" localhost:8083/connectors/
+-- curl -H "Accept:application/json" localhost:8083/connectors/
 
- curl -s http://localhost:8083/connectors/postgres-connector/status
-INSERT INTO cuentas (id, estado, fecha_creacion, saldo, usuario_id) VALUES (1, 'activo', '2025-02-23', 1000.50, 123);
+--  curl -s http://localhost:8083/connectors/postgres-connector/status
+-- INSERT INTO cuentas (id, estado, fecha_creacion, saldo, usuario_id) VALUES (1, 'activo', '2025-02-23', 1000.50, 123);
 
 
 
-INSERT INTO consumos (id, cuenta_id, descripcion, estado, fecha_consumo, monto) VALUES (1, 13, 'Compra en tienda', 'activo', '2023-10-25 14:30:00', 100.50);
+-- INSERT INTO consumos (id, cuenta_id, descripcion, estado, fecha_consumo, monto) VALUES (1, 13, 'Compra en tienda', 'activo', '2023-10-25 14:30:00', 100.50);
